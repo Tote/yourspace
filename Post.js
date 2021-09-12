2ff6da7a97dfa880ab7b668c643a3c24ef515fd5
Patricio Morel <totenbot@outlook.com>
2021-09-12 06:39:04 -0300

    Refactored into Post class

---
+++ Post.js
@@ -0,0 +1,26 @@
+class Post{
+    postElem
+    likesElem
+    likesCounter = 0
+    constructor(type, topic){
+        this.type = type
+        this.topic = topic 
+        this.postElem   = document.createElement('div')
+        activePosts.insertBefore(this.postElem, activePosts.firstChild)
+        this.render()
+    }
+
+    render(){
+        this.postElem.innerHTML = `
+            Your ${this.type} about ${this.topic} is active
+            <span>
+                ${this.likesCounter} likes
+            </span>
+        `
+    }
+
+    addLikes(likes){
+        this.likesCounter += likes
+        this.render()
+    }
+}
--- posts.js
+++ posts.js
@@ -1,25 +1,6 @@
-function createPost(type, topic){
-    const post      = document.createElement('div') 
-    const likesInfo = document.createElement('div')
-    let likeCounter = 0
-
-    const updateLikes = likes => {
-        likeCounter += likes
-        likesInfo.textContent = `${likeCounter} likes`
-    } 
-
-    post.textContent = `Your ${type} about ${topic} is active`
-    post.appendChild(likesInfo)
-    updateLikes(0)
-
-    activePosts.insertBefore(post, activePosts.firstChild)
-    return updateLikes
-}
 function postText(){
-    const post = createPost('text', 'animals')
-    console.log(post)
-    
-    distributeLikes(1000000, 14, post)
+    const post = new Post('text', 'dogs')
+    distributeLikes(1000000,14,post.addLikes.bind(post))
 }
 
 textPost.onclick = (e,v) => postText()
