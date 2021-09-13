51fb185864e16c1a2d9925cc5e1e33dd6f215b64
Patricio Morel <totenbot@outlook.com>
2021-09-13 01:06:22 -0300

    First Trending Event List

---
+++ trending.js
@@ -0,0 +1,57 @@
+const TRENDING_TOPICS = [
+    'Fake News',
+    // Natural Disasters
+    'Hurricane in South Quinteshire',
+    'A 7.5 Earthquake',
+    'Vaccination',
+    
+    //Activism
+    '#PetSpace',
+    '#ClimateChange',
+    '#EarthDay',
+    '',
+    //others
+    '#ShowYourSpace',
+    
+    //The Venegas
+    '#TheVenegasNewEpisode',
+    '#TheVenegasWilliamAgainstHilda',
+    '#TheVenegasGoodbyeHilda',
+    '#TheVenegasMonchoSavesTheDay',
+    '#TheVenegasAreBack',
+    '#TheVenegasSeasonFinale',
+    '#TheVenegasSeriesFinale',
+
+    //WhiteGreen
+    'WHITEGREEN Debut Album',
+    'WHITEGREEN In Your Space',
+    'WHITEGREEN feat. Chayanne',
+    'WHITEGREEN feat. DuaLinga',
+    'WHITEGREEN feat. Selenium Gomez',
+    'WHITEGREEN feat. B-a-cardi',
+    'WHITEGREEN feat. Miss Radio Gaga',
+
+    '#WHITEGREENNewSong',
+    '#WHITEGREENTheConcert',
+    '#WHITEGREENPhotobook',
+    '#WHITEGREENComeback',
+ 
+    //FairnessAlliance
+    'Fairness Alliance Movie',
+    '#FairnessAlliance',
+    '#FairnessAllianceOnTheaters',
+    'Fairness Alliance Directors Cut',
+    'Fairness Alliance: Batguy v Superguy',
+    'Awesome Woman Movie',
+    'Awesome Woman is Back',
+    'Awesome Woman delayed',
+    'Awesome Woman 1984',
+    'Waterman',
+    'Fairness Alliance new movie',
+    '#FairnessAllianceTheMusical',
+    
+    
+
+
+
+]
